acde3f51b374f8f9d765875df8be5f7cd45d60ba
Austin Chen <akrolsmir@gmail.com>
2023-01-13 20:18:49 -0800

    Write about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,5 +3,30 @@
 </script>
 
 <div class="flex justify-center my-32 text-2xl">
-	<p>This is the about page.</p>
+	<div class="prose">
+		<h3 id="what-is-this-">What is this?</h3>
+		<p>
+			GoodLedger is a database of Effective Altruism grants. Find out how much funding your favorite
+			charities have received!
+		</p>
+		<h3 id="where-is-the-donation-data-from-">Where is the donation data from?</h3>
+		<p>
+			All donation data comes from Vipul Naik’s <a href="https://github.com/vipulnaik/donations"
+				>Donation List Website</a
+			>.
+		</p>
+		<p>
+			The source code for GoodLedger is <a href="https://github.com/rachelweinberg12/zoe"
+				>available on Github</a
+			>.
+		</p>
+		<h3 id="some-donations-are-wrong-or-missing-">Some donations are wrong or missing!</h3>
+		<p>
+			Sorry! We’d like to allow people to fix and add new donations to the database, in the future.
+			Stay tuned!
+		</p>
+		<h3 id="who-built-this-">Who built this?</h3>
+		<p>Rachel Weinberg, with help from Austin Chen.</p>
+		<p>If you have questions or feedback, reach out to rachel.weinberg12@gmail.com!</p>
+	</div>
 </div>
