8ef4e38e6413ffb1fd9bb8914afe8d9d44591384
Austin Chen <akrolsmir@gmail.com>
2023-01-15 19:00:18 -0800

    Add umami analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,15 @@
 	import '$lib/app.css';
 </script>
 
+<svelte:head>
+	<script
+		async
+		defer
+		src="https://analytics.umami.is/script.js"
+		data-website-id="e0653aa1-fe3b-4659-9de1-32d36d802e19"
+	></script>
+</svelte:head>
+
 <div class="app">
 	<Header />
 
