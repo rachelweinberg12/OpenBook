cd566f15f6ab97c839a615271a2ef680b73aab13
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-01-30 09:29:17 -0800

    Fix max-w on link columns so table doesn't go over right side of page

--- src/lib/TdLink.svelte
+++ src/lib/TdLink.svelte
@@ -4,7 +4,7 @@
 
 <td
 	{...$$restProps}
-	class="hover:underline hidden lg:table-cell min-w-full text-left px-6 truncate overflow-hidden ..."
+	class="hover:underline hidden lg:table-cell max-w-xs min-w-full text-left px-6 truncate overflow-hidden ..."
 >
 	<a href={url}><slot /></a></td
 >
