3c4488164c9fa1afe60f9bca1c8c2bcac855f87a
rachelweinberg12 <rachel.weinberg12@gmail.com>
2023-02-01 18:14:21 -0800

    Fix download

---
+++ src/lib/Button.svelte
@@ -0,0 +1,8 @@
+<button
+	on:click
+	{...$$restProps}
+	class={'bg-violet-400 w-44 py-2 px-3 rounded-md shadow-sm hover:cursor-pointer hover:shadow-xl text-white relative bottom-1' +
+		$$restProps.class}
+>
+	<slot />
+</button>
--- src/lib/DataDownload.svelte
+++ src/lib/DataDownload.svelte
@@ -1,7 +1,7 @@
 <a
-	class="bg-violet-400 w-44 py-2 px-3 rounded-md shadow-sm hover:cursor-pointer hover:shadow-xl text-white relative bottom-1"
-	href="$static/OpenBookDonations.csv"
-	download
+	class="bg-violet-400 w-36 py-2 px-3 rounded-md shadow-sm hover:cursor-pointer hover:shadow-xl text-white relative bottom-1"
+	href="https://rachelweinberg12.files.wordpress.com/2023/02/donations_rows.xlsx"
+	download="OpenBookDonations"
 	><svg
 		xmlns="http://www.w3.org/2000/svg"
 		fill="none"
@@ -16,5 +16,5 @@
 			d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
 		/>
 	</svg>
-	Download CSV</a
+	Download</a
 >
